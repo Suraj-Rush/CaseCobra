ee1bb64d3f76a2370444865572f83c39c7fbca62
Surajglitch <suraj.ug21@nsut.ac.in>
2024-08-27 22:05:42 +0530

    Revert ""changed price""
    This reverts commit d3942775e680c2b27736e48b8dc1347dbed9dad8.

--- src/config/products.ts
+++ src/config/products.ts
@@ -1,12 +1,12 @@
 export const PRODUCT_PRICES = {
   material: {
     silicone: 0,
-    polycarbonate: 100_00,
+    polycarbonate: 5_00,
   },
   finish: {
     smooth: 0,
-    textured: 100_00,
+    textured: 3_00,
   },
 } as const
 
-export const BASE_PRICE = 399_00
+export const BASE_PRICE = 14_00
